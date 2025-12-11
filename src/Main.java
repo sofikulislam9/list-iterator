12b76719e403c3380f6b2420332ce86cc8a67044
Sofikul <sofikulislam9757@gmail.com>
2025-12-11 09:13:33 +0530

    Added new file

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    List<Integer> list = new ArrayList<>();
+
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+
+    System.out.println("Forward");
+
+    ListIterator<Integer> lrt = list.listIterator();
+
+    while (lrt.hasNext()) {
+        System.out.print(lrt.next() + " ");
+    }
+
+    System.out.println("\nBackward");
+
+    while (lrt.hasPrevious()) {
+        System.out.print(lrt.previous() + " ");
+    }
+
+    }
+
